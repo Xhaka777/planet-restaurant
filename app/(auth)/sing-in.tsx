d3d5b31ba21dd53db03021e709f4ca5d6372c7f4
Altin Xhaka <altinxhaka1@gmail.com>
2024-09-30 15:39:29 +0200

    Calling FormField component at the signIn screen

--- app/(auth)/sing-in.tsx
+++ app/(auth)/sing-in.tsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from "react"
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, ScrollView, Dimensions, Alert, Image } from 'react-native'
 import { useNavigation } from "@react-navigation/native";
+import FormField from "@/components/FormField";
 
 const SignIn = () => {
     const [isSubmitting, setSubmitting] = useState(false);
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+
+    const navigation = useNavigation()
 
     return (
         <SafeAreaView className='h-full'>
@@ -20,6 +23,22 @@ const SignIn = () => {
                     <Text className="text-2xl font-semibold text-black mt-10 font-psemibold">
                         Log in to<Text className="text-secondary-200">Planet Acc</Text>
                     </Text>
+
+                    <FormField
+                        title="Email"
+                        value={email}
+                        handleChangeText={setEmail}
+                        otherStyles="mt-7"
+                        placeholder="Enter your email"
+                    />
+
+                    <FormField
+                        title="Password"
+                        value={password}
+                        handleChangeText={setPassword}
+                        otherStyles="mt-7"
+                        placeholder="Enter your password"
+                    />
 
                     <View className="flex justify-center t-5 flex-row gap-2">
                         <Text className="text-lg text-black-100 font-pregular">
