1b4fe8b152c4b581a228b45c5103d932fe9d887f
Altin Xhaka <altinxhaka1@gmail.com>
2024-09-30 13:57:49 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+     "./**/*.{js,jsx,ts,tsx]"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // primary: "#161622",
+        primary: '#f8f4f4',
+        secondary: {
+          DEFAULT: "#705cb4",
+          100: "#705cb4",
+          200: "#705cb4",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        gray: {
+          100: "#CDCDE0",
+          300:'#d1d5db'
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
